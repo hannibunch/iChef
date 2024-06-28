a7961ee9d619e86bb43a81a1d60d39637186b973
hannibunch <kurosewataru@gmail.com>
2024-06-28 16:49:33 +0200

    Main Page done
    Main Page done

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,10 +1,16 @@
-document.getElementById('saveButton').addEventListener('click', function() {
-    const icon = this.querySelector('i');
-    if (icon.classList.contains('fa-regular')) {
-        icon.classList.remove('fa-regular');
-        icon.classList.add('fa-solid');
-    } else {
-        icon.classList.remove('fa-solid');
-        icon.classList.add('fa-regular');
-    }
+document.querySelectorAll('.save-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        const icon = this.querySelector('i');
+        if (icon.classList.contains('fa-regular')) {
+            icon.classList.remove('fa-regular');
+            icon.classList.add('fa-solid');
+        } else {
+            icon.classList.remove('fa-solid');
+            icon.classList.add('fa-regular');
+        }
+    });
 });
+
+function recipeClick() {
+  window.location.href = 'pages/recipe-page.html';
+}
